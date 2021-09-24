fc8311cf61f91b2baaec25c4853a59c4b0f554a4
JuampiXD <50757995+JuampiXD@users.noreply.github.com>
2021-09-24 19:48:05 -0400

    Implementando HTML.

---
+++ app/swater.py
@@ -0,0 +1,11 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    responses = requests.get('https://75jwlvujpd.execute-api.us-east-2.amazonaws.com/staging/expeditions')
+    expedition = eval(responses.text)
+    return render_template('index.html', data=expedition)
