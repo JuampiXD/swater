485f9fdc27f58108218f09ceb4e5af676d1d37ce
JuampiXD <50757995+JuampiXD@users.noreply.github.com>
2021-09-26 18:46:53 -0400

    Implementando la funcion drinkable()

--- app/swater.py
+++ app/swater.py
@@ -1,13 +1,23 @@
+from random import randrange
 import requests
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def get():
     responses = requests.get('https://75jwlvujpd.execute-api.us-east-2.amazonaws.com/staging/expeditions')
     expedition = eval(responses.text)
-    return render_template('index.html', data=expedition)
+    expedition['cheker'] = drinkable()
+    return render_template('index.html', expedition=expedition)
 
+
+def drinkable ():
+    checker = randrange(2)
+    print(checker)
+    return checker
+
+
+# def pull(*expedition):
+#     return render_template('index.html', expedition=expedition)
